dff4cbcf08301e8b55ba2674136d8e7c43606ee4
Jun Young <9811hjy@gmail.com>
2020-07-21 14:41:40 +0000

    1012

---
+++ Baekjoon/class3/1012.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct node Node;
+
+struct node{
+	//int size;
+	//int dum;
+	int value;
+	int cor[2];
+	struct node *next;
+	struct node *prev;
+	struct node *up;
+	struct node *down;
+
+};
+
+int main(){
+	
+	int T;
+	
+	cin >> T;
+	
+	for(int i = 0; i < T; i ++){
+		Node * head;	
+		head = (Node*)malloc(sizeof(Node));
+		head->value = 0;
+		head->cor = {0,0};
+		
+		int M, N, K;
+		
+		cin >> M >> N >> K;
+		
+		for(int i = 0; i < M; i ++){
+			for(int j = 0; j < N; j ++){
+				
+				Node * n_node, tmp;
+				if(i == 0 && j == 0){
+					n_node = head;	
+					
+				}
+				else{
+					n_node = (Node*)malloc(sizeof(Node));
+					n_node->cor = {i,j};
+				}
+				tmp = n_node;
+				
+				
+			}
+			
+		}
+		
+	
+		for (int j = 0; j < K; j ++){
+			
+		}
+		
+		
+	}
+	
+	
+}
