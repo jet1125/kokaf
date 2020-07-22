3e4c740c56a7b8f33b2991e017a2a165aff20cae
Jun Young <9811hjy@gmail.com>
2020-07-22 14:52:21 +0000

    pointer

--- test/test1.cpp
+++ test/test1.cpp
@@ -4,21 +4,37 @@ using namespace std;
 
 
 int main(){
-	int *x = nullptr;
-	int n;
-	std::cin >> n;
-	x = new int[n];  //x = (int*)malloc(sizeof(int)*5);
+	int** x = nullptr;
+	int m,n;
+	std::cin >> m >> n;
+	x = new int* [m];  //x = (int*)malloc(sizeof(int)*5);
+	for(int i =0; i < m; i ++){
+		x[i] = new int[m];
+	}
+	
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			x[i][j] = i*n +j;
+		}
+	}
+	
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			std::cout << x[i][j]<<" ";
+		}
+		std::cout << std::endl;
+	}
 
-	for (int i = 0; i < n; i++)
-	{
-		x[i] = 1;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		std::cout<<x[i]<<" ";
-	}
-	std::cout << std::endl;
-	delete[] x;
+	for (int i = 0; i < m; i++)
+		delete [] x[i];
+	delete [] x;
+
+
+
 
 }
 
