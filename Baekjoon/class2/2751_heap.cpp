46c906d7cf568f5df1cabcbed23c39159c31dec4
jet1125 <9811hjy@gmail.com>
2020-08-09 14:50:36 +0000

    heap

--- Baekjoon/class2/2751_heap.cpp
+++ Baekjoon/class2/2751_heap.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #define SWAP(x, y, temp) ( (temp)=(x), (x)=(y), (y)=(temp) )
 
 using namespace std;
@@ -6,12 +7,17 @@ using namespace std;
 void heapify(int arr[], int p, int heapsize){
 	int left = p * 2;
 	int right = p * 2 + 1;
+	int largest;
 	int temp;
-	if(left <= heapsize && arr[left-1] > arr[p-1])
-		SWAP(arr[left-1], arr[p-1], temp);
+	largest = p;
+	if(left <= heapsize && arr[left-1] < arr[largest-1])
+		//SWAP(arr[left-1], arr[p-1], temp);
+		largest = left;
+	if(right <= heapsize && arr[right-1] < arr[largest-1])
+		largest = right;
 	
-	if(right <= heapsize && arr[right-1] > arr[p-1])
-		SWAP(arr[right-1], arr[p-1], temp);
+	if(largest != p)
+		SWAP(arr[largest-1], arr[p-1], temp);
 	
 }
 
@@ -28,24 +34,29 @@ void build_heap(int arr[], int heapsize){
 int main(){
 	
 	int N, temp;
-	 cin >> N;
+	//cin >> N;
+	scanf("%d", &N);
 	int * arr = nullptr;
 	arr = new int[N];
 	
 	for(int i = 0; i < N; i ++){
-		cin >> arr[i];
-		
+		//cin >> arr[i];
+		int k;
+		scanf("%d", &k);
+		arr[i] = k;
 	}
 	for(int i = N; i > 0; i --){
 		
 		build_heap(arr, i);
 		//cout << arr[0] << endl;
+		printf("%d\n", arr[0]);
 		SWAP(arr[0], arr[i - 1], temp);
 		//cout << arr[N-i];	
+		
 	}
 	
-	for (int i = 0; i < N; i ++)
-		cout << arr[i] << "\n";
+	delete[] arr;
+	
 	
 	
 }
---
+++ Baekjoon/class3/7662.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	
+	int T;
+	
+	cin >> T;
+	for(int i = 0; i < T; i ++){
+		int k;
+		cin >> k;
+		int * arr = nullptr;
+		arr = new int[k];
+		
+		
+	}
+	
+}
