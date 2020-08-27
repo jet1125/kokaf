438265eb33a1c0baab2429f65013d46c4ddd2dc0
jet1125 <9811hjy@gmail.com>
2020-08-27 14:41:45 +0000

    1074

--- Baekjoon/class3/1074.cpp
+++ Baekjoon/class3/1074.cpp
@@ -3,49 +3,42 @@
 
 using namespace std;
 
-int move_x[4] = {0, 1, -1, 1};
-int move_y[4] = {0, 0, 1, 0};
-
 int main(){
 	int N;
 	int a;
 	int b;
 	cin >> N >> a >> b;
-	int **arr = nullptr;
-	int size = pow(int(2), int(N)); 
-	arr = new int*[size];
-	
-	for(int i= 0; i < size; i ++){
-		arr[i] = new int[size];
-	}
-	
-	//int cnt = 0;
-	int cur_x = 0;
-	int cur_y = 0;
-	int k = 0;
-	int q = 0;
-	int x = 0;
-	int y = 0;
-	for(int i = 0; i < size * size; i ++){
-		k = i % 4;
-		q = i / 4;
-		if(k == 0){
-			cur_x = cur_x + (move_x[q] * N);
-			cur_y = cur_y + (move_y[q] * N); 
-			x = cur_x;
-			y = cur_y;
+	int size = pow(int(2), int(N));
+	int k = size;
+	int s = 0;
+	int cnt = 0;
+	while(k > 0){
+		if(a < (k / 2) && b < (k / 2)){
+			s = 0;
+			
 		}
-		x = x + move_x[k];
-		y = y + move_y[k];
-		arr[x][y] = i;
-	
+			
+		else if(a < (k / 2) && b >= (k / 2)){
+			s = 1;
+			b = b - k / 2;
+		}
+			
+		else if(a >= (k / 2) && b < (k / 2)){
+			s = 2;
+			a = a - k / 2;
+		}
+			
+		else if(a >= (k / 2) && b >= (k / 2)){
+			s = 3;
+			a = a - k / 2;
+			b = b - k / 2;
+		}
+			
+		cnt = cnt + s * (k / 2) * (k / 2);
+		
+		k = k / 2;
 	}
+	
+	cout << cnt;
 
-	cout << arr[a][b];
-	// for(int i = 0; i < size; i ++){
-	// 	for(int j = 0; j < size; j ++){
-	// 		cout << arr[j][i] << " ";
-	// 	}
-	// 	cout << endl;
-	// }
 }
